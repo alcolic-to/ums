a753181554d52c7aa2947fbf8efdc3fd2b601b67
Milan Topalovic <mtopalovic994@gmail.com>
2024-08-16 21:59:26 +0200

    Adding more checks for EventTest

--- test/event_test.cpp
+++ test/event_test.cpp
@@ -13,10 +13,15 @@ ConditionalEvent e3;
 
 #define ENDL '\n'
 
+std::mutex m;
+std::vector<std::uint8_t> v;
+
 void f1()
 {
     std::cout << "ENTERED f1" << ENDL;
     e1.wait();
+    std::lock_guard<std::mutex> lock(m);
+    v.push_back(1);
     std::cout << "EXITED f1" << ENDL;
 }
 
@@ -24,6 +29,8 @@ void f2()
 {
     std::cout << "ENTERED f2" << ENDL;
     e2.wait();
+    std::lock_guard<std::mutex> lock(m);
+    v.push_back(2);
     std::cout << "EXITED f2" << ENDL;
 }
 
@@ -31,27 +38,32 @@ void f3()
 {
     std::cout << "ENTERED f3" << ENDL;
     e3.wait();
+    std::lock_guard<std::mutex> lock(m);
+    v.push_back(3);
     std::cout << "EXITED f3" << ENDL;
 }
 
 TEST(Event, SimpleTest)
 {
-    std::uint64_t start = get_time_in_ms();
-
     task_manager.execute_task<true>(f1);
     task_manager.execute_task<true>(f2);
     task_manager.execute_task<true>(f3);
 
     std::this_thread::sleep_for(std::chrono::milliseconds(1000 /* miliseconds */));
-    e1.signal();
+    e2.signal();
 
     std::this_thread::sleep_for(std::chrono::milliseconds(1000 /* miliseconds */));
-    e2.signal();
+    e1.signal();
 
     std::this_thread::sleep_for(std::chrono::milliseconds(1000 /* miliseconds */));
     e3.signal();
 
-    std::uint64_t end = get_time_in_ms();
-    
-    std::cout << "Total exec time: " << end - start << "ms." << ENDL;
+    // Wait for the threads to finish.
+    std::this_thread::sleep_for(std::chrono::milliseconds(2000 /* miliseconds */));
+
+    std::lock_guard<std::mutex> lock(m);
+    ASSERT_EQ(v.size(), 3);
+    ASSERT_EQ(v[0], 2);
+    ASSERT_EQ(v[1], 1);
+    ASSERT_EQ(v[2], 3);
 }

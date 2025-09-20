299e7b7707af8daef10dfbe162c5da4cfdc4a168
aleksandarcolic22414 <acacolic@gmail.com>
2025-09-20 22:23:50 +0200

    Set libc++ by default.

--- cmake/compiler_options.cmake
+++ cmake/compiler_options.cmake
@@ -6,7 +6,15 @@ else()
     endif()
 endif()
 
-SET(CMAKE_CXX_FLAGS  "-Wall")
+# Next section is compiler options.
+
+# Optional, using libc++ for better debugging with lldb-dap.
+OPTION(UMS_USE_LIBCPP "If set, sets compiler stdlib value to libc++." ON)
+if (UMS_USE_LIBCPP)
+    SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++")
+endif()
+
+SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall")
 
 if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
     SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-interference-size")

c904948205416360a46e30efedf77d39755dd8a7
alcolic-to <acacolic@gmail.com>
2025-11-12 02:12:28 +0100

    Added apache licence, v2.0

--- util.cpp
+++ util.cpp
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2025, Aleksandar Colic
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 #include "util.h"
 
 #include <cstdint>
@@ -11,6 +26,10 @@
 
 namespace ums {
 
+/**
+ * Random number generator.
+ * Taken from stockfish.
+ */
 class PRNG {
 public:
     explicit PRNG(u64 seed = u64(now().time_since_epoch().count())) noexcept : m_seed{seed} {}

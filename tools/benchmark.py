2afdd03d3a1c31f29fed0a2114b0965da1b66714
alcolic-to <acacolic@gmail.com>
2025-11-20 11:16:06 +0100

    Improved bechmark.

--- tools/benchmark.py
+++ tools/benchmark.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 
 import os
+import re
 import argparse
 import platform
 import subprocess
 import sys
 import tempfile
+import warnings
 from contextlib import chdir
 from distutils.dir_util import copy_tree, remove_tree
 
@@ -43,29 +45,40 @@ def execs_in_dir(directory : str):
 # Main workflow
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="top", description="Parse filters")
-    parser.add_argument("-f", "--filter", nargs="?", default="BM", help="filter regex for benchmark")
+    parser.add_argument("-f", "--filter", nargs="?", default=".*", help="filter regex for benchmark files")
     args = parser.parse_args()
+
+    if not args.filter.startswith(".*"):
+        args.filter = ".*" + args.filter
+
+    if not args.filter.endswith(".*"):
+        args.filter = args.filter + ".*"
+
+    print("benchmarks filter: ", args.filter)
 
     # main
     root = run_command("git rev-parse --show-toplevel")
 
-    bm_baseline_branch = "master"
-    bm_test_branch = run_command("git rev-parse --abbrev-ref HEAD")
-
-    tmp_dir = tempfile.gettempdir()
-    bm_baseline_dir = tmp_dir + "/baseline_benchmark"
-    bm_test_dir = tmp_dir + "/test_benchmark"
-
     # Create benchmarks and copy them to tmp folder.
     with chdir(root):
-        cfg_cmd = "cmake -B build -S . -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -G Ninja -DUMS_BUILD_BENCHMARK=1"
+        bm_baseline_branch = "master"
+        bm_contender_branch = run_command("git rev-parse --abbrev-ref HEAD")
+
+        if bm_contender_branch == bm_baseline_branch:
+            print("\033[93m" + "WARNING: running benchmarks on the same branch: " + bm_baseline_branch + "\033[0m")
+
+        tmp_dir = tempfile.gettempdir()
+        bm_baseline_dir = tmp_dir + "/baseline_benchmark"
+        bm_contender_dir = tmp_dir + "/contender_benchmark"
+
+        cfg_cmd = "cmake -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -G Ninja -DUMS_BUILD_BENCHMARK=1"
         build_cmd = "cmake --build build --config Release"
         run_command(cfg_cmd)
         run_command(build_cmd)
 
-        rm_dir(bm_test_dir)
+        rm_dir(bm_contender_dir)
 
-        copy_tree("build/bin/Release/benchmark", bm_test_dir)
+        copy_tree("build/bin/Release/benchmark", bm_contender_dir)
 
         run_command("git checkout " + bm_baseline_branch)
 
@@ -76,7 +89,7 @@ if __name__ == "__main__":
 
         copy_tree("build/bin/Release/benchmark", bm_baseline_dir)
 
-        run_command("git checkout " + bm_test_branch)
+        run_command("git checkout " + bm_contender_branch)
 
     # Compare benchmarks.
     with chdir(tmp_dir):
@@ -84,10 +97,12 @@ if __name__ == "__main__":
 
         # Running benchmarks
         for exe in execs_in_dir(bm_baseline_dir):
+            if not re.match(args.filter, exe):
+                continue
+
             bm_baseline_exe = bm_baseline_dir + "/" + exe
-            bm_test_exe = bm_test_dir + "/" + exe
-            run_command(compare_py_script + " benchmarksfiltered " + bm_baseline_exe + " " + args.filter + " " + bm_test_exe + " " + args.filter , cout=True)
+            bm_contender_exe = bm_contender_dir + "/" + exe
+            run_command(compare_py_script + " benchmarks " + bm_baseline_exe + " " + bm_contender_exe, cout=True)
         
         remove_tree(bm_baseline_dir)
-        remove_tree(bm_test_dir)
-    
+        remove_tree(bm_contender_dir)

c3973041ea9c22bc3337f9afd0ab374c745cc80d
alcolic-to <acacolic@gmail.com>
2025-11-27 08:53:00 +0100

    Modified STL lib cached variable.

--- cmake/stl.cmake
+++ cmake/stl.cmake
@@ -1,19 +1,23 @@
+# Sets include directory for external STL lib.
+# User can provide custom STL lib path if it is already installed by setting UMS_STL_PATH variable.
 set(UMS_STL_PATH "" CACHE STRING "Path to STL common lib.")
-if (NOT UMS_STL_PATH STREQUAL "")
+if (UMS_STL_PATH STREQUAL "")
+    include(FetchContent)
+
+    FetchContent_Declare(
+        stl
+        GIT_REPOSITORY https://github.com/alcolic-to/stl.git
+        GIT_SHALLOW    TRUE
+        GIT_PROGRESS   TRUE)
+
+    FetchContent_MakeAvailable(stl)
+
+    message("Using internal STL lib ${stl_SOURCE_DIR} for UMS.")
+    set(UMS_STL_PATH ${stl_SOURCE_DIR} CACHE STRING "Path to STL common lib." FORCE)
+    message("UMS_STL_PATH value: ${UMS_STL_PATH}")
+else ()
+    set(UMS_STL_PATH ${UMS_STL_PATH} CACHE STRING "Path to STL common lib." FORCE)
     message("Using external STL lib ${UMS_STL_PATH} for UMS.")
-    target_include_directories(${UMS_LIB} SYSTEM PUBLIC ${UMS_STL_PATH})
-    return()
-endif()
+endif ()
 
-include(FetchContent)
-
-FetchContent_Declare(
-    stl
-    GIT_REPOSITORY https://github.com/alcolic-to/stl.git
-    GIT_SHALLOW    TRUE
-    GIT_PROGRESS   TRUE)
-
-FetchContent_MakeAvailable(stl)
-
-message("Using internal STL lib ${stl_SOURCE_DIR} for UMS.")
-target_include_directories(${UMS_LIB} SYSTEM PUBLIC ${stl_SOURCE_DIR})
+target_include_directories(${UMS_LIB} SYSTEM PUBLIC ${UMS_STL_PATH})
